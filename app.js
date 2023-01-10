2864589d4278e25a59ef8895309c21378745078e
kim <kog3480@naver.com>
2023-01-10 18:01:23 +0900

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 alert("hi");
 console.log("hello");
+
+let a=5;
+const b=2;
+const myName="kim";
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log("hello "+myName);
+
+a=7;
+console.log(a/b)
+
+const anIFant = true;
+console.log(anIFant);
+
+const amIFant = null;
+let something;
+console.log(amIFant);
+console.log(something);
+
+const daysOfWeek=["mon","tue","wed","thu","fir","sat"];
+
+//Get Item form Array
+console.log(daysOfWeek[5])
+
+//Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
